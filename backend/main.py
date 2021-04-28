44be86d8ff8dc7d9d246bebba7b7d07a20fe85c7
nofoobar <ilovedjango3000@gmail.com>
2021-04-28 22:02:58 +0530

    Add remaining files for db connection

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
 from core.config import settings
+from db.session import engine
+from db.base_class import Base
 
-app = FastAPI(title=settings.PROJECT_TITLE,version=settings.PROJECT_VERSION)
+
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+
+def start_application():
+    app = FastAPI(title=settings.PROJECT_TITLE,version=settings.PROJECT_VERSION)
+    create_tables()
+    return app
+
+app = start_application()
 
 
 @app.get("/")
