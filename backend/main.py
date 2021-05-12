adb06e6fbbbace7a1abaf3c380a32b2afd951acb
nofoobar <ilovedjango3000@gmail.com>
2021-05-12 06:37:17 +0530

    serve static files

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,7 @@ from db.session import engine
 from db.base import Base
 from apis.base import api_router
 from webapps.base import api_router as webapp_router
+from fastapi.staticfiles import StaticFiles
 
 
 def create_tables():
@@ -13,12 +14,17 @@ def create_tables():
 def include_router(app):
     app.include_router(api_router)
     app.include_router(webapp_router)
+
+
+def configure_static(app):
+    app.mount("/static",StaticFiles(directory="static"),name="static")
     
 
 def start_application():
     app = FastAPI(title=settings.PROJECT_TITLE,version=settings.PROJECT_VERSION)
     create_tables()
     include_router(app)
+    configure_static(app)
     return app
 
 app = start_application()
