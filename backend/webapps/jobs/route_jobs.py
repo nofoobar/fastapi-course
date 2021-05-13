0b22fe58100a360b8057a9256e9e1140647107a8
nofoobar <ilovedjango3000@gmail.com>
2021-05-13 05:58:54 +0530

    Add support for listview in webapp

--- backend/webapps/jobs/route_jobs.py
+++ backend/webapps/jobs/route_jobs.py
@@ -2,6 +2,10 @@ from fastapi import APIRouter
 from fastapi import Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from db.repository.jobs import list_jobs
+from sqlalchemy.orm import Session
+from db.session import get_db
+from fastapi import Depends
 
 
 templates = Jinja2Templates(directory="templates")
@@ -9,5 +13,6 @@ router = APIRouter(include_in_schema=False)
 
 
 @router.get("/")
-def home(request:Request):
-    return templates.TemplateResponse("jobs/homepage.html",{"request":request})
+def home(request:Request,db:Session = Depends(get_db)):
+    jobs = list_jobs(db=db)
+    return templates.TemplateResponse("jobs/homepage.html",{"request":request,"jobs":jobs})
