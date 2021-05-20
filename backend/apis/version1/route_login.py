4b2767b87ba035eaef5962868730b8ecd8d57443
nofoobar <ilovedjango3000@gmail.com>
2021-05-20 16:45:54 +0530

    Use HttpOnly cookie to store token

--- backend/apis/version1/route_login.py
+++ backend/apis/version1/route_login.py
@@ -1,9 +1,11 @@
-from fastapi.security import OAuth2PasswordRequestForm,OAuth2PasswordBearer
+from fastapi.security import OAuth2PasswordRequestForm
+from apis.utils import OAuth2PasswordBearerWithCookie
 from fastapi import Depends,HTTPException,status
 from fastapi import APIRouter
 from sqlalchemy.orm import Session
 from datetime import timedelta
 from jose import jwt,JWTError
+from fastapi import Response
 
 from db.session import get_db
 from db.repository.login import get_user
@@ -25,7 +27,7 @@ def authenticate_user(username:str,password:str,db:Session):
 
 
 @router.post("/token")
-def login_for_access_token(form_data:OAuth2PasswordRequestForm=Depends(),
+def login_for_access_token(response :Response,form_data:OAuth2PasswordRequestForm=Depends(),
     db:Session = Depends(get_db)):
     user = authenticate_user(form_data.username,form_data.password,db)
     if not user:
@@ -33,10 +35,11 @@ def login_for_access_token(form_data:OAuth2PasswordRequestForm=Depends(),
         detail="Incorrect username or password")
     access_token_expire = timedelta(minutes= settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(data={"sub":user.email},expires_delta=access_token_expire)
+    response.set_cookie(key="access_token",value=f"Bearer {access_token}",httponly=True)
     return {"access_token":access_token,"token_type":"bearer"}
 
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login/token")
+oauth2_scheme = OAuth2PasswordBearerWithCookie(tokenUrl="/login/token")
 
 
 def get_current_user_from_token(token:str = Depends(oauth2_scheme),db:Session=Depends(get_db)):
